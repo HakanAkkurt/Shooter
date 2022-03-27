ae5b1b7c09753b41cf833532827e9280bca04c4b
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2022-03-27 01:05:29 +0100

    Pickup widget dynamic values

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -50,6 +50,14 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	class USphereComponent* AreaSphere;
 
+	// The name which appears on the Pickup Widget
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	FString ItemName;
+
+	// Item count (ammo, etc.)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	int32 ItemCount;
+
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
 };
