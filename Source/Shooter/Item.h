d49d4e80bb4a5b04075fdbdf0c412e71dbc982a7
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2022-03-28 01:35:30 +0200

    Item Rarity Stars

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -6,6 +6,19 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+UENUM(BlueprintType)
+enum class EItemRarity : uint8
+{
+	EIR_Damaged UMETA(DisplayName = "Damaged"),
+	EIR_Common UMETA(DisplayName = "Common"),
+	EIR_Uncommon UMETA(DisplayName = "Uncommon"),
+	EIR_Rare UMETA(DisplayName = "Rare"),
+	EIR_Legendary UMETA(DisplayName = "Legendary"),
+
+	EIR_MAX UMETA(DisplayName = "DefaultMax")
+
+};
+
 UCLASS()
 class SHOOTER_API AItem : public AActor
 {
@@ -28,6 +41,9 @@ protected:
 	UFUNCTION()
 	void OnSpehereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex);
+
+	// Sets the ActiveStars array of bools based on rarity
+	void SetActiveStars();
 
 public:	
 	// Called every frame
@@ -55,8 +71,15 @@ private:
 	FString ItemName;
 
 	// Item count (ammo, etc.)
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	int32 ItemCount;
+
+	// Item rarity - determines number of starts in Pickup Widget
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	EItemRarity ItemRarity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	TArray<bool> ActiveStars;
 
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
