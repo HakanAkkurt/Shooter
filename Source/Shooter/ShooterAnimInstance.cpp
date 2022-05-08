f9329628360be588920f312e438917a4f7f875b7
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2022-05-09 01:15:01 +0200

    Recoil effect fix

--- Source/Shooter/ShooterAnimInstance.cpp
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -21,7 +21,9 @@ UShooterAnimInstance::UShooterAnimInstance() :
 	RootYawOffset(0.f),
 	Pitch(0.f),
 	bReloading(false),
-	OffsetState(EOffsetState::EOS_Hip)
+	OffsetState(EOffsetState::EOS_Hip),
+	RecoilWeight(1.0f),
+	bTurningInPlace(false)
 {
 	
 }
@@ -115,6 +117,7 @@ void UShooterAnimInstance::TurnInPlace()
 		const float Turning{ GetCurveValue(TEXT("Turning")) };
 		if (Turning > 0) {
 
+			bTurningInPlace = true;
 			RotationCurveLastFrame = RotationCurve;
 			RotationCurve = GetCurveValue(TEXT("Rotation"));
 			const float DeltaRotation{ RotationCurve - RotationCurveLastFrame };
@@ -127,6 +130,44 @@ void UShooterAnimInstance::TurnInPlace()
 
 				const float YawExcess{ ABSRootYawOffset - 90.f };
 				RootYawOffset > 0 ? RootYawOffset -= YawExcess : RootYawOffset += YawExcess;
+			}
+		}
+		else {
+
+			bTurningInPlace = false;
+		}
+
+		if (bTurningInPlace) {
+
+			if (bReloading) {
+
+				RecoilWeight = 1.f;
+			}
+			else {
+				RecoilWeight = 0.f;
+			}
+
+		}
+		else { // Not turning in place
+			if (bCrouching) {
+
+				if (bReloading) {
+
+					RecoilWeight = 1.f;
+				}
+				else {
+					RecoilWeight = 0.1f;
+				}
+			}
+			else {
+
+				if (bAiming || bReloading) {
+
+					RecoilWeight = 1.f;
+				}
+				else {
+					RecoilWeight = 0.5f;
+				}
 			}
 		}
 
